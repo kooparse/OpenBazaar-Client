b10324e87217b0f3295d6dc9f74f64df6c3734c9
Brian Hoffman <brianchoffman@gmail.com>
2015-12-21 00:07:37 -0500

    Re-enable Copy Paste

--- main.js
+++ main.js
@@ -274,6 +274,7 @@ app.on('ready', function() {
 
   autoUpdater.on("error", function(err, msg) {
     console.log(msg); //print msg , you can find the cash reason.
+    mainWindow.webContents.executeJavaScript("console.log('test: " + msg + "');");
   });
 
   autoUpdater.on("update-not-available", function(msg) {
@@ -299,6 +300,7 @@ app.on('ready', function() {
 
   autoUpdater.setFeedURL('http://updates.openbazaar.org:5000/update/' + platform + '/' + version);
   autoUpdater.checkForUpdates();
+
 
 });
 
